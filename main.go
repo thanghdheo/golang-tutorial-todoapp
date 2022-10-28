049fe5795ee8d4317ada44563d566daa7a34523b
THẮNG <thang.hd@novaintechs.com>
2022-10-28 22:28:22 +0700

    done-grammar

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -29,9 +30,14 @@ func main() {
 	{
 		todo := v1.Group("/todo")
 		{
-			todo.POST("/create",createTodo(db))
+			todo.POST("/create", createTodo(db))
+			todo.GET("/:id", getTodo(db))
+			todo.GET("", getTodos(db))
+			todo.PUT("/update/:id", updateTodo(db))
+			todo.PUT("/delete/:id", deleteTodo(db))
 		}
 	}
+	router.Run(":3000")
 }
 
 type Todo struct {
@@ -44,15 +50,29 @@ type Todo struct {
 }
 
 func (Todo) TableName() string {
-	return "todogr"
+	return "todo"
 }
 
 type TodoCreate struct {
-	Id    int    `json:"id" gorm:"id;"`
-	Title string `json:"title" gorm:"title;"`
+	Id        int        `json:"id" gorm:"id;"`
+	Title     string     `json:"title" gorm:"title;"`
+	CreatedAt *time.Time `json:"created_at" gorm:"column:created_at;"`
+	UpdatedAt *time.Time `json:"updated_at" gorm:"column:updated_at;"`
+}
+
+type TodoUpdate struct {
+	Id        *int       `json:"id" gorm:"id;"`
+	Title     *string    `json:"title" gorm:"title;"`
+	Deleted   bool       `json:"deleted" gorm:"deleted;"`
+	UpdatedAt *time.Time `json:"updated_at" gorm:"column:updated_at;"`
 }
 
 func (TodoCreate) TableName() string {
+	return Todo{}.TableName()
+}
+
+
+func (TodoUpdate) TableName() string {
 	return Todo{}.TableName()
 }
 
@@ -79,8 +99,121 @@ func createTodo(db *gorm.DB) gin.HandlerFunc {
 	}
 }
 
+func getTodo(db *gorm.DB) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		id, err := strconv.Atoi(ctx.Param("id"))
+
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var data Todo
+
+		if err := db.Where("id = ?", id).First(&data).Error; err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"data": data})
+
+	}
+}
+
+func getTodos(db *gorm.DB) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		type Paging struct {
+			Page  int   `json:"page" form:"page;"`
+			Limit int   `json:"limit" form:"limit;"`
+			Total int64 `json:"total" form:"-"`
+		}
+
+		var paging Paging
+
+		if err := ctx.ShouldBind(&paging); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if paging.Page == 0 {
+			paging.Page = 1
+		}
+
+		if paging.Limit <= 10 {
+			paging.Limit = 10
+		}
+
+		offset := (paging.Page - 1) * paging.Limit
+
+		var data []Todo
+
+		if err := db.Table(Todo{}.TableName()).Count(&paging.Total).Offset(offset).Find(&data).Error; err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"data": data, "paging": paging})
+
+	}
+}
+
+func updateTodo(db *gorm.DB) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var data TodoUpdate
+
+		id, err := strconv.Atoi(ctx.Param("id"))
+
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		data.Deleted = false
+
+		if err := ctx.ShouldBind(&data); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err := db.Where("id = ?", id).Updates(&data).Error; err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"data": true})
+	}
+}
+
+func deleteTodo(db *gorm.DB) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var data TodoUpdate
+
+		id, err := strconv.Atoi(ctx.Param("id"))
+
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		data.Deleted = true
+
+		if err := ctx.ShouldBind(&data); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err := db.Where("id = ?", id).Updates(&data).Error; err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"data": true})
+	}
+}
+
 func (res *TodoCreate) Validate() error {
 	res.Id = 0
+	res.UpdatedAt = nil
 	res.Title = strings.TrimSpace(res.Title)
 
 	if len(res.Title) == 0 {
